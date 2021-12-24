4c9892c4ae01b94d9842aeb94613cabad74a350f
Oleksandr Shvets <oleksandr.shvets.ua@gmail.com>
2021-12-24 19:05:47 +0200

    add some currency

--- logic/data.ts
+++ logic/data.ts
@@ -1,6 +1,6 @@
 
 export const currencies = [
-    "UAH", "EUR", "USD", "GBP", "JPY", "AUD", 
+    "UAH", "EUR", "USD", "GBP", "CHF", "JPY", "CAD", "AUD", "NZD", "SGD", 
 ] as const
 
 export const percents = [1, 2, 3, 4, 5]
